9a58e22f10fd56f08f0e08edb93fc531705baca4
jens <jens@MacBook-Pro-van-jens.local>
2026-03-27 19:52:29 +0100

    fix: use supported firebase auth import for react-native persistence

--- app/lib/firebase-auth.ts
+++ app/lib/firebase-auth.ts
@@ -6,6 +6,7 @@ import {
   GoogleAuthProvider,
   OAuthProvider,
   createUserWithEmailAndPassword,
+  getReactNativePersistence,
   initializeAuth,
   onAuthStateChanged,
   signInWithCredential,
@@ -13,7 +14,6 @@ import {
   signOut,
   type User,
 } from "firebase/auth";
-import { getReactNativePersistence } from "firebase/auth/react-native";
 
 import { ENV } from "@/constants/env";
 
